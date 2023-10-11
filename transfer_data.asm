f0ea423c78e5ff3900986a0d58103a343b0d30f3
Snevin Leoneel Dsouza <81520536+snevindsouza@users.noreply.github.com>
2023-10-11 18:02:19 +0530

    Add files via upload

---
+++ transfer_data.asm
@@ -0,0 +1,11 @@
+; In this program R0 and R1 registers are used as memory pointers. A count values in register R5 is 
+; loaded to repeat the data transfer recursively. 
+MOV R0,#10H 	; SOURCE MEMORY 
+MOV R1,#20H 	; DESTINATION MEMORY 
+MOV R5,#0AH 	; COUNTER 
+UP: MOV A, @R0 	; DATA TRANSFER OF A BYTE 
+	MOV @R1, A 
+	INC R0 
+	INC R1 
+	DJNZ R5, UP ; REPEAT THE PROCESS 
+END
