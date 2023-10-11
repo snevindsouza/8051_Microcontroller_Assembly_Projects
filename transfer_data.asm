0c8af6a12185253140eb28db70700bbd60a90851
Snevin Leoneel Dsouza <81520536+snevindsouza@users.noreply.github.com>
2023-10-11 18:44:50 +0530

    Update transfer_data.asm

--- transfer_data.asm
+++ transfer_data.asm
@@ -1,4 +1,5 @@
 ; In this program R0 and R1 registers are used as memory pointers. A count values in register R5 is loaded to repeat the data transfer recursively. 
+; We are going to transfer 10 data bytes from 10H to 20H locations
 MOV R0,#10H 	; SOURCE MEMORY 
 MOV R1,#20H 	; DESTINATION MEMORY 
 MOV R5,#0AH 	; COUNTER 
