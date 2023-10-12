84456f3ed1726e6a104e1de4b7e4512170e3d4c9
Snevin Leoneel Dsouza <81520536+snevindsouza@users.noreply.github.com>
2023-10-12 06:08:22 +0530

    Update transfer_data_external.asm

--- transfer_data_external.asm
+++ transfer_data_external.asm
@@ -1,7 +1,7 @@
 ; DPTR is used as a memory pointer for the external data memory. 
 ; MOVX instruction is used to achieve the data transfer. 
-MOV R2, #21H    ; Store the higher byte of DPTR (DPH &DPL) ( 2100) 2100,2101,2102….. 
-MOV R3, #57H    ; store the higher byte of DPTR (5700) 5700,5701,….. 
+MOV R2, #21H    ; Store the higher byte of DPTR (DPH &DPL) ( 2100) 2100,2101,2102...
+MOV R3, #57H    ; store the higher byte of DPTR (5700) 5700,5701...
 MOV R4, #10     ; Counter for 10 data bytes to transfer 
 MOV DPTR, #2100H 
 UP: MOVX A, @DPTR   ; reads the ex-memory 
