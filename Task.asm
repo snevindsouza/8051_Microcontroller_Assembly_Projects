50f2025e4f428031107a3dab4e7b31ba8c5a9d94
Snevin Leoneel Dsouza <81520536+snevindsouza@users.noreply.github.com>
2023-10-21 16:44:12 +0530

    Task.asm

--- Task.asm
+++ Task.asm
@@ -1,19 +1,25 @@
+;8051 assembly program that sorts batch-1 data bytes provided, stored in the internal memory starting from 10H in assending order.
+;Sorts batch-2 data bytes provided, stored in external memory starting from 1000H in descending order.
+;Save the assending order result in the internal memory starting from 20H
+;Save the descendin order result in the external memory starting from 2000H
+
 ; Sort batch 1 data
 MOV R3,#10 				
 DEC R3 						
 NEXT_2: MOV R0,#10H 	
-		MOV R2,03H 			
+	MOV R2,03H 			
 NEXT_1:	MOV A,@R0 			
-		INC R0
- 		MOV 40H,@R0
-		CJNE A,40H,CHECK 	
+	INC R0
+ 	MOV 40H,@R0
+	CJNE A,40H,CHECK 	
 CHECK: 	JC NO_EXCHANGE 		
-		XCH A, @R0 			
-		DEC R0 				
-		MOV @R0,A
-		INC R0
+	XCH A, @R0 			
+	DEC R0 				
+	MOV @R0,A
+	INC R0
 NO_EXCHANGE: DJNZ R2, NEXT_1 
-			 DJNZ R3, NEXT_2
+	     DJNZ R3, NEXT_2
+
 ; save batch 1 data			  
 MOV R0,#10H 
 MOV R1,#20H 
@@ -23,29 +29,31 @@ UP: MOV A,@R0
 	INC R0
 	INC R1
 	DJNZ R5, UP
+
 ; sort batch 2 data
 MOV R3,#10 				
 DEC R3 						
 NEXT_4: MOV DPTR,#1000H 	
-		MOV R2,03H 			
+	MOV R2,03H 			
 NEXT_3:	MOVX A,@DPTR			
-		INC DPTR
-		MOV @R0, A
-		MOVX A, @DPTR
- 		MOV @R1, A
-		MOV A, @R0
-		MOV 40H, @R1
-		CJNE A, 40H, CHECK1 	
+	INC DPTR
+	MOV @R0, A
+	MOVX A, @DPTR
+ 	MOV @R1, A
+	MOV A, @R0
+	MOV 40H, @R1
+	CJNE A, 40H, CHECK1 	
 CHECK1: JNC NO_EXCHANGE1 		
-		XCH A, @R1 			
-		DEC DPL 				
-		MOVX @DPTR,A
-		INC DPTR
-		MOV A, @R1
-		MOVX @DPTR, A  
+	XCH A, @R1 			
+	DEC DPL 				
+	MOVX @DPTR,A
+	INC DPTR
+	MOV A, @R1
+	MOVX @DPTR, A  
 NO_EXCHANGE1: DJNZ R2, NEXT_3 
-			  DJNZ R3, NEXT_4
-;sort batch 2 data
+	      DJNZ R3, NEXT_4
+
+;save batch 2 data
 MOV R2, #10H 
 MOV R3, #20H 	
 MOV R4, #10 
